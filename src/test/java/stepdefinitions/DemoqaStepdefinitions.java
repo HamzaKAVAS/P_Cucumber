73ffedae766f501e68c6affab0855a2ec17e8840
HamzaKAVAS <hmzkavas@gmail.com>
2024-12-29 23:33:01 +0300

    demoq

--- src/test/java/stepdefinitions/DemoqaStepdefinitions.java
+++ src/test/java/stepdefinitions/DemoqaStepdefinitions.java
@@ -2,6 +2,7 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -14,6 +15,7 @@ import java.time.Duration;
 public class DemoqaStepdefinitions {
 
     DemoqaPage demoqaPage = new DemoqaPage();
+    WebDriverWait wait;
 
     @Then("soldaki sekmelerden Alerts'e tiklar")
     public void soldaki_sekmelerden_alerts_e_tiklar() {
@@ -27,17 +29,18 @@ public class DemoqaStepdefinitions {
 
     @When("Allert’in gorunur olmasini bekler")
     public void allert_in_gorunur_olmasini_bekler() {
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(demoqaPage.onButtonClickMe));
+        wait = new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.alertIsPresent());
     }
 
     @Then("Allert uzerindeki yazinin {string} oldugunu test edin")
-    public void allert_uzerindeki_yazinin_oldugunu_test_edin(String expectedYazi) {
-
+    public void allert_uzerindeki_yazinin_oldugunu_test_edin(String expectedAlertYazi) {
+        String actualAlertYazi = Driver.getDriver().switchTo().alert().getText();
+        Assertions.assertEquals(expectedAlertYazi,actualAlertYazi);
     }
 
     @Then("Ok diyerek alerti kapatir")
     public void ok_diyerek_alerti_kapatir() {
-
+        Driver.getDriver().switchTo().alert().accept();
     }
 }
